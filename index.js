a4f97d47d889f9cc906c02be914b31e1df351b4d
gabriel Carino <gabejcarino@gmail.com>
2022-04-03 20:28:17 -0500

    commit tree fc33956b90185deb74466c172c7e3cbe2cb5afa4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    for (const win of record) {
+        if (win.result === "W") {
+            return win.year;
+     }
+    }
+}
+console.log(record.find(superbowlWin).year)
